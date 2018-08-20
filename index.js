4916f32eacbdd60680e3e9ca975247f4190d506e
christophergoot <christophergoot@gmail.com>
2018-08-20 14:57:04 -0700

    tripple double

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-console.log('hello world');
-
 // inserts a certain number of words "WUB" before the first word of the song (the number may be zero), after the last word (the number may be zero), and between words (at least one between any pair of neighbouring words), and then the boy glues together all the words, including "WUB", in one string and plays the song at the club.
 
 // For example, a song with words "I AM X" can transform into a dubstep remix as "WUBWUBIWUBAMWUBWUBX" and cannot transform into "WUBWUBIAMWUBX".
@@ -9,13 +7,35 @@ function songDecoder(song){
 	words.forEach(word => {
 		if (word !== '') {
 			orig += word + ' ';
-			console.log('word', word);
 		}
 	})
 	return orig.trim();
   }
 
 
+//  which takes in numbers num1 and num2 and returns 1 if there is a straight triple of a number at any place in num1 and also a straight double of the same number in num2.
+function tripledouble(num1, num2) {
+	let output = 0
+	const trippleNums = [];
+	const ints = (''+num1).split('');
+	for (let i=0; i<ints.length; i++) {
+		if (ints[i] === ints[i+1] && ints[i] === ints[i+2]) {
+			trippleNums.push(ints[i]);
+		}
+	}
+	if (trippleNums.length > 0) {
+		const ints2 = (''+num2).split('');
+		trippleNums.forEach(num => {
+			for (let i=0; i<ints2.length; i++) {
+				if (ints2[i] === num && ints2[i+1] === num) output = 1;
+			}
+		})
+	}
+	return output;
+ }
+
+
 module.exports = {
-	songDecoder
+	songDecoder,
+	tripledouble,
 }
--- test/test.js
+++ test/test.js
@@ -1,14 +1,21 @@
 const { 
 	songDecoder,
+	tripledouble
  } = require('../index');
 
 const assert = require('assert');
 describe('Code Wars', function() {
-  describe('songDecoder()', function() {
     it('WUB WUB Should WUB', function() {
 	  assert.equal(songDecoder("AWUBBWUBC"), "A B C","WUB should be replaced by 1 space");
 	  assert.equal(songDecoder("AWUBWUBWUBBWUBWUBWUBC"), "A B C","multiples WUB should be replaced by only 1 space");
 	  assert.equal(songDecoder("WUBAWUBBWUBCWUB"), "A B C","heading or trailing spaces should be removed");
-    });
-  });
+    }),
+  	it('trippleDouble', function() {
+		assert.equal(tripledouble(451999277,41177722899),1);
+		assert.equal(tripledouble(1222345, 12345),0);
+		assert.equal(tripledouble(12345, 12345),0);
+		assert.equal(tripledouble(666789, 12345667),1);
+		assert.equal(tripledouble(10560002, 100),1);
+		assert.equal(tripledouble(662137333777555500, 2885957744),1);
+	});
 });
