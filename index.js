835965d11ff7e0f6ab5cdc6c93871f9e06d30d01
christophergoot <christophergoot@gmail.com>
2018-08-27 12:57:04 -0700

    Split Strings

--- index.js
+++ index.js
@@ -34,8 +34,18 @@ function tripledouble(num1, num2) {
 	return output;
  }
 
+ function solution(str){
+	let arr = [];
+	for (let i=0; i<str.length; i=i+2) {
+	  if (str[i+1]) arr.push(`${str[i]}${str[i+1]}`);
+	  else arr.push(`${str[i]}_`);
+	}
+	return arr;
+ }
+
 
 module.exports = {
 	songDecoder,
 	tripledouble,
+	solution
 }
--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 const { 
 	songDecoder,
-	tripledouble
+	tripledouble,
+	solution
  } = require('../index');
 
 const assert = require('assert');
@@ -17,5 +18,9 @@ describe('Code Wars', function() {
 		assert.equal(tripledouble(666789, 12345667),1);
 		assert.equal(tripledouble(10560002, 100),1);
 		assert.equal(tripledouble(662137333777555500, 2885957744),1);
-	});
+	}),
+	it('Split Strings', () => {
+		assert.deepEqual(solution('abc'),['ab','c_']);
+		assert.deepEqual(solution('abcdef'),['ab', 'cd', 'ef']);
+	})
 });
