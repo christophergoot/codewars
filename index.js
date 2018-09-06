08eae2b06906a947f582a1221feafefda83b0c2d
christophergoot <christophergoot@gmail.com>
2018-09-06 13:34:14 -0700

    parse deadfish solution

--- index.js
+++ index.js
@@ -92,10 +92,40 @@ function fullBubbleSort(arr) {
 	return (newArr);
 }
 
+// Write a simple parser that will parse and run Deadfish.
+// Deadfish has 4 commands, each 1 character long:
+// i increments the value (initially 0)
+// d decrements the value
+// s squares the value
+// o outputs the value into the return array
+// Invalid characters should be ignored.
+
 function parse(str) {
 	const arr = [];
+	let value = 0;
 	for (let i=0; i<str.length; i++) {
-		arr.push(str.charAt(i));
+		switch(str.charAt(i)) {
+			case 'i': {
+				value ++;
+				break;
+			}
+			case 'd': {
+				value --;
+				break;
+			}
+			case 's': {
+				value = value * value;
+				break;
+			}
+			case 'o': {
+				arr.push(value);
+				break;
+			}				
+
+			default: {
+				break;
+			}
+		}
 	}
 	return arr;
 }
